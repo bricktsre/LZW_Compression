7905fdc90d7d0054822c0025f0a4aff986fee3c0
Greg <Greg@192.168.0.104>
2018-11-05 00:55:22 -0500

    Now fully implements character by character encoding

--- LZWmod.java
+++ LZWmod.java
@@ -1,8 +1,11 @@
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /*************************************************************************
@@ -20,35 +23,54 @@ public class LZWmod {
     private static final int R = 256;        // number of input chars
     private static final int L = 4096;       // number of codewords = 2^W
     private static final int W = 12;         // codeword width
-    private static BufferedReader scan;
+    //private static int buffer =0;
+    //private static int blen = 0;
+    //private static BufferedReader scan;
+    //private static BufferedOutputStream bout;
+    private static BinaryIn infile;
+    private static BinaryOut outfile;
 
     public static void compress() throws IOException { 
         RTrie<Integer> st = new RTrie<Integer>();
         for (int i = 0; i < R; i++)
             st.put(new StringBuilder("" + (char) i), i);
         int code = R+1;  // R is codeword for EOF
-
+        
         StringBuilder input = new StringBuilder();
-        input.append((char)scan.read());//BinaryStdIn.readChar());
-        while (input.length() > 0) {
-            StringBuilder s = input;
-            boolean first = true;
-        	while(s.length()==input.length()) {
-        		if(!first)
-        			input.append((char)scan.read());//BinaryStdIn.readChar());
-        		else
-        			first=false;
-        		s= st.longestPrefixOf(input);
-        	}
-            BinaryStdOut.write(st.get(s), W);      // Print s's encoding.
+        input.append(infile.readChar());//BinaryStdIn.readChar());
+        while (!infile.isEmpty()) {
+            input = nextStringBuilder(input,st);
+            StringBuilder s = st.longestPrefixOf(input);
+        	//write(st.get(s));
+        	//BinaryStdOut.write(st.get(s), W);      // Print s's encoding.
+            outfile.write(st.get(s),W);
             int t = s.length();
             if (t < input.length() && code < L)    // Add s to symbol table.
                 st.put(input, code++);
             input.delete(0, input.length()-1);            // Scan past s in input.
         }
-        BinaryStdOut.write(R, W);
-        BinaryStdOut.close();
+        outfile.write(R,W);
+        outfile.close();
+        //BinaryStdOut.write(R, W);
+        //BinaryStdOut.close();
     } 
+    
+    private static StringBuilder nextStringBuilder(StringBuilder input, RTrie<Integer> st) {
+    	StringBuilder s = input;
+        boolean first = true;
+    	while(s.length()==input.length()) {
+    		if(!first) {	
+    			try {
+    				input.append(infile.readChar());	//BinaryStdIn.readChar());
+    			}catch(NoSuchElementException e) {
+    				return input;
+    			}
+    		}else
+    			first=false;
+    		s= st.longestPrefixOf(input);
+    	}
+    	return input;
+    }
 
     public static void expand() {
         String[] st = new String[L];
@@ -74,18 +96,45 @@ public class LZWmod {
         BinaryStdOut.close();
     }
 
-
-
+    /*private static void write(int a) throws IOException {
+    	if(blen<W) {
+    		buffer |= a;
+    		blen+=W;
+    	}else {
+    		buffer= buffer<< W;
+    		buffer |= a;
+    		for(int i=0;i<3;i++) {
+    			byte temp = (byte) buffer;
+    			bout.write(temp);
+    			buffer = buffer >>8;
+    		}
+    		blen=0;
+    	}
+    }
+    
+    private static void close() throws IOException {
+    	if(blen!=0) {
+    		bout.write((byte)buffer);
+    		bout.write((byte)buffer>>8);		
+    	}
+    	bout.close();
+    }*/
+    
     public static void main(String[] args) throws IOException {
-       // if      (args[0].equals("-")) compress();
-        //else if (args[0].equals("+")) expand();
-        //else throw new RuntimeException("Illegal command line argument");
-        try {
-			scan= new BufferedReader(new FileReader("code.txt"));
+    	/*if      (args[0].equals("-")) compress();
+        else if (args[0].equals("+")) expand();
+        else throw new RuntimeException("Illegal command line argument");
+        */
+        /*try {
+			scan= new BufferedReader(new FileReader("wacky.bmp"));
+        	FileOutputStream fout = new FileOutputStream("wacky.lzwm"); 
+	        bout = new BufferedOutputStream(fout);     	
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
+        infile = new BinaryIn("frosty.jpg");
+    	outfile = new BinaryOut("frosty.lzwm");
         compress();
     }
 
